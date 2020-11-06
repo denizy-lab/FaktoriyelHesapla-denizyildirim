fe41e0148b10a4eae93692a50705c5cd3e172e2a
IR0NNTi <IR0NNTi@DESKTOP-960OI32>
2020-11-07 02:04:40 +0300

    FaktoriyelHesapla

---
+++ Faktoriyel/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Faktoriyel
+{
+    class Program
+    {
+        private static int sonuc = 1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faktoriyelini Alacağın Sayıyı Gir: ");
+            int sayi =Convert.ToInt32 (Console.ReadLine());
+            for(int i = 1; i<=sayi; i++)
+            {
+                sonuc = sonuc * i;
+            }     
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+        }
+    }
+}
